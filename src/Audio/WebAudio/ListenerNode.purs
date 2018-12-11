6259f6cf5c852bb1d38ea914dfd23d67423de582
Mario Lang <mlang@delysid.org>
2018-12-11 11:58:47 +0100

    ListenerNode.set{Position,Orientation}

--- src/Audio/WebAudio/ListenerNode.purs
+++ src/Audio/WebAudio/ListenerNode.purs
@@ -1,8 +1,11 @@
 module Audio.WebAudio.ListenerNode
   ( positionX, positionY, positionZ
   , forwardX, forwardY, forwardZ
-  , upX, upY, upZ ) where
+  , upX, upY, upZ
+  , setPosition, setOrientation ) where
 
+import Prelude (Unit, discard, (=<<))
+import Audio.WebAudio.AudioParam (setValue)
 import Audio.WebAudio.Types (AudioParam, ListenerNode)
 import Effect (Effect)
 
@@ -15,3 +18,18 @@ foreign import forwardZ :: ListenerNode -> Effect AudioParam
 foreign import upX :: ListenerNode -> Effect AudioParam
 foreign import upY :: ListenerNode -> Effect AudioParam
 foreign import upZ :: ListenerNode -> Effect AudioParam
+
+setPosition :: Number -> Number -> Number -> ListenerNode -> Effect Unit
+setPosition x y z l = do
+  setValue x =<< positionX l
+  setValue y =<< positionY l
+  setValue z =<< positionZ l
+
+setOrientation :: Number -> Number -> Number -> Number -> Number -> Number -> ListenerNode -> Effect Unit
+setOrientation fx fy fz ux uy uz l = do
+  setValue fx =<< forwardX l
+  setValue fy =<< forwardY l
+  setValue fz =<< forwardZ l
+  setValue ux =<< upX l
+  setValue uy =<< upY l
+  setValue uz =<< upZ l
